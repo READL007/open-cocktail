a66aa713ee16da5e1d1ce18ebbcfdcd51260b0d7
eleonoreeuzenes <eleonore.euzenes@gmail.com>
2026-03-02 12:13:59 +0100

    feat: create cocktail module and its model

---
+++ cocktails/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Cocktail model with name, category, preparation time, ingredients, steps, number of servings, difficulty, ustensils
+class Cocktail(models.Model):
+    TYPE_CHOICES = [
+        ('dry', 'Dry'),
+        ('sugary', 'Sugary'),
+        ('alcohol_free', 'Alcohol free'),
+    ]
+
+    DIFFICULTY = [
+        ('easy', 'Easy'),
+        ('medium', 'Medium'),
+        ('hard', 'Hard'),
+    ]
+
+    name = models.CharField(max_length=255)
+    category =  models.CharField(max_length=20, choices=TYPE_CHOICES)
+    preparation_time = models.IntegerField()
+    ingredients = models.TextField()
+    steps = models.TextField()
+    servings = models.IntegerField()
+    difficulty = models.CharField(max_length=20, choices=DIFFICULTY)
+    ustensils = models.TextField()
+
+    def __str__(self):
+        return self.name
