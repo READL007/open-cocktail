e0ff2f2f41d81a3651f853c4dbc6b74e508f451f
eleonoreeuzenes <eleonore.euzenes@gmail.com>
2026-03-02 13:33:41 +0100

    feat: list views

--- cocktails/views.py
+++ cocktails/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# List all cocktails
+def cocktail_list(request):
+    return render(request, 'cocktails/cocktail_list.html')
+
+# View cocktail details
+def cocktail_detail(request, cocktail_id):
+    return render(request, 'cocktails/cocktail_detail.html', {'cocktail_id': cocktail_id})
